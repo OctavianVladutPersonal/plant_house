462c432f3d748ffc18c661c1e90e2985e29f9175
Octavian Vladut <vladut.octavian@ymail.com>
2025-11-28 22:03:58 +0200

    update database

--- firebase-config.js
+++ firebase-config.js
@@ -3,13 +3,13 @@
 // Get these from Firebase Console: Project Settings > General > Your apps > Web app
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCKlzm9Om4oMAoG2z5-g7aqGVnazpyeTc4",
-  authDomain: "book-library-95091.firebaseapp.com",
-  projectId: "book-library-95091",
-  storageBucket: "book-library-95091.firebasestorage.app",
-  messagingSenderId: "976306987324",
-  appId: "1:976306987324:web:6293fb860f4ee23045be39",
-  measurementId: "G-846KQJ3T7B"
+  apiKey: "AIzaSyAaOP47iTLRvd2KDih4u0PxhscHRvLomx0",
+  authDomain: "plant-house-b407c.firebaseapp.com",
+  projectId: "plant-house-b407c",
+  storageBucket: "plant-house-b407c.firebasestorage.app",
+  messagingSenderId: "27881234890",
+  appId: "1:27881234890:web:1347b0ac84262c0f00ef33",
+  measurementId: "G-H94B3YQLCW"
 };
 
 // Initialize Firebase (using compat version)
